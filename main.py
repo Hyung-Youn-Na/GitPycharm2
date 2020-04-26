2232ccb4ab3b238e48c55259f0f0c17751fb654e
Hyung-Youn-Na <47072202+Hyung-Youn-Na@users.noreply.github.com>
2020-04-26 19:32:39 +0900

    Update 2 on GitHub Branch Only

--- main.py
+++ main.py
@@ -23,3 +23,6 @@ print("Hello World Updates on GitHub branch ")
 
 #adding new item in master branch
 print("Hello World Updates on Local Master Branch")
+
+#updates in main branch of GitHub
+print("Hello World Updates 2 on GitHub branch Only ")
